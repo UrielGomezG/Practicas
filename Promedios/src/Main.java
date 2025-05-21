6f394467ea46c1c995de37d0a9c6326bd79782d0
UrielGomezG <20233tn177@utez.edu.mx>
2025-05-21 08:39:50 -0600

    Correccion de la carpeta subida

---
+++ Promedios/src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String x = JOptionPane.showInputDialog("Dame un dato");
+        String y = JOptionPane.showInputDialog("Dame un segundo dato");
+        String z = JOptionPane.showInputDialog("Dame un tercer dato");
+
+        double numero1 = Integer.parseInt(x);
+        double numero2 = Integer.parseInt(x);
+        double numero3 = Integer.parseInt(x);
+        double promedio = (numero1 + numero2 + numero3)/3;
+
+        System.out.println("El promedio es: " + promedio);
+    }
+}
